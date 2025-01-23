f5b7ed821e49dca834a944b0c53c723a13b703fa
Landododo <1108landon@gmail.com>
2025-01-23 16:50:18 -0500

    faster gui

--- get_inputs.py
+++ get_inputs.py
@@ -3,17 +3,15 @@ from tkinter import *
 from tkinter import filedialog
 import pyautogui
 
-
-def open_file():
-    file_path = filedialog.askopenfilename(title = "File Selector",
-                                           filetypes=(("gds files","*.gds"),
-                                            ("all files","*.*")))
-window = Tk()
-button = Button(text= "Open", command= open_file)
-button.pack()
-window.mainloop()
+def get_inputs():
+    """Uses tkinter to prompt the user to select a GDS file"""
+    file_path = filedialog.askopenfilename(title = "Select a GDS file",
+                                            filetypes=(("gds files","*.gds"),))
+    qr_code_count, qr_size, qrs_per_row, qrs_per_col = count_qr_codes(file_path)
+    print(qr_code_count)
 
 if __name__ == "__main__":
-    gds_file_path = "file.gds"  # Replace with your .gds file path
-    qr_code_count = count_qr_codes(gds_file_path)
-    print(f"Number of QR codes in the GDS file: {qr_code_count}")
+    get_inputs()
+    # gds_file_path = "file.gds"  # Replace with your .gds file path
+    # qr_code_count = count_qr_codes(gds_file_path)
+    # print(f"Number of QR codes in the GDS file: {qr_code_count}")
