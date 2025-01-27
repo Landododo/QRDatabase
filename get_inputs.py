2e80585bc2ff09b34eebbd9a58e67abae8f0f269
Landododo <1108landon@gmail.com>
2025-01-26 22:39:25 -0500

    added feature that stores previous input folders so you dont need to reselect

--- get_inputs.py
+++ get_inputs.py
@@ -4,12 +4,45 @@ from tkinter import filedialog
 from QR_Selector import qr_selector
 import pyautogui
 
-def get_inputs():
+def get_previous_inputs():
+    """Load previous inputs from a input txt file"""
+    try:
+        with open("user_data.txt", 'r') as f:
+            return f.read()
+    except:
+        return None
+    
+
+def get_file_paths():
     """Uses tkinter to prompt the user to select a GDS file"""
     file_path = filedialog.askopenfilename(title = "Select a GDS file",
-                                            filetypes=(("gds files","*.gds"),))
-    qr_code_count, qr_size, qrs_per_row, qrs_per_col = count_qr_codes(file_path)
+                                        filetypes=(("gds files","*.gds"),))
     sample_folder = filedialog.askdirectory(title = "Select directory storing sample images")
+    with open("user_data.txt", 'w') as f:
+        f.write(file_path + "\n" + sample_folder)
+    return file_path, sample_folder
+def get_inputs():
+    previous = get_previous_inputs()
+    file_path, sample_folder = None, None
+    if previous:
+        print(previous)
+        file_path, sample_folder = previous.split("\n")
+        win = Tk()
+        win.geometry("750x250")
+        entry= Message(win, text=f"Do you want to proceed with previously inputted gds file {file_path} and sample folder {sample_folder}?", width= 400)
+        entry.pack()
+        buttons = Frame(win)
+        buttons.pack(pady = 5)
+        button1= Button(buttons, text= "Yes", command=win.destroy)
+        button1.pack(side = LEFT)
+        button2= Button(buttons, text= "No", command = lambda: [win.destroy(), get_file_paths()])
+        button2.pack()
+        win.mainloop()
+    else:
+        file_path, sample_folder = get_file_paths()
+
+    print(file_path + "checking" + sample_folder)
+    qr_code_count, qr_size, qrs_per_row, qrs_per_col = count_qr_codes(file_path)
     print("Reading file: " + file_path.split("/")[-1])
     print("Reading samples from: " + sample_folder)
     qr_selector(qrs_per_row, qrs_per_col)
