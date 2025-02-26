bc780eff2660ce91718b9ce384bd09a8092eb661
Landododo <1108landon@gmail.com>
2025-02-26 17:04:19 -0500

    made zip files work

--- QR_database/home/data_handlers.py
+++ QR_database/home/data_handlers.py
@@ -8,6 +8,7 @@ import os
 import zipfile
 from detection.main import get_detections
 import cv2,random, sys, re
+from pathlib import Path
 
 def handle_uploaded_file(file):
     """Handles an uploaded gds file by uploading the file to
@@ -33,49 +34,77 @@ def handle_uploaded_file(file):
     new_file.save()
 
 
+def add_sample_to_db(file_name, id, directory_path):
+    """adds an individual file to the database"""
+    detections = get_detections(directory_path + file_name)
+    print(detections)
+    im = cv2.imread(directory_path + file_name)
+    print(im)
+    with connection.cursor() as cursor:
+        # get information from the gds file (used to calculate qr code position)
+        cursor.execute("SELECT qr_size, spacing, padding FROM home_gds_files WHERE id = %s", [id])
+        qr_size, spacing, padding = cursor.fetchall()[0]
+        rand_id = random.randint(-sys.maxsize-1, sys.maxsize)
+        unique_id = False
+        while unique_id == False:
+            cursor.execute("Select * FROM home_gds_files WHERE id = %s", [rand_id])
+            info = cursor.fetchall()
+            if info == []:
+                unique_id = True
+            else:
+                rand_id = random.randint(-sys.maxsize-1, sys.maxsize)
+
+        print("bouta add")
+        for detection in detections:
+            print("detected")
+            sample_file = sample_images()
+            sample_file.gds_file_id = id
+            sample_file.file_name = file_name
+            sample_file.width = im.shape[1]
+            sample_file.height = im.shape[0]
+            if "." not in detection.payload:
+                sample_file.row, sample_file.col = int(re.search( ",(.*):U=UL",detection.payload).group(1)), int(detection.payload.split(",")[0])
+                sample_file.abs_x = sample_file.row * (qr_size + spacing) + padding
+                sample_file.abs_y = sample_file.col * (qr_size + spacing) + padding
+
+            else:
+                sample_file.abs_x, sample_file.abs_y = int(re.search(",(.*):U=")),int(detection.payload.split(",")[0])
+                sample_file.row = (sample_file.abs_x - padding) / (qr_size + spacing)
+                sample_file.col = (sample_file.abs_y - padding) / (qr_size + spacing)
+            sample_file.num_codes = len(detections)
+            sample_file.img_id = rand_id # unique id for each image that is the same regardless of which qr code is stored
+            sample_file.is_anchor = True
+            sample_file.layer = 1
+            sample_file.save()
+            print(sample_file)
+
 def handle_sample_file(file, id):
+    """Takes in a file and the id of the gds file that it is a sample to,
+    and adds that file to the sample folder with the id of the gds file,
+    and adds the sample file(s) to the database as well. Handles multiple files
+    contained in a single .zip or single files in .png, .jpeg, .jpg, or .jp2 formats
+    """
     directory_path = str(BASE_DIR) + "/home/uploads/samples/id=" + id + "/"
     if not os.path.exists(directory_path):
         os.mkdir(directory_path)
     if ".zip" in file.name:
         with zipfile.ZipFile(file, 'r') as zip_ref:
             zip_ref.extractall(directory_path)
-    else:
-        with open(directory_path + file.name, 'wb+') as f:
-            for chunk in file.chunks():
-                f.write(chunk)
-        detections = get_detections(directory_path + file.name)
-        im = cv2.imread(directory_path + file.name)
-        with connection.cursor() as cursor:
-            # get information from the gds file (used to calculate qr code position)
-            cursor.execute("SELECT qr_size, spacing, padding FROM home_gds_files WHERE id = %s", [id])
-            qr_size, spacing, padding = cursor.fetchall()[0]
-            rand_id = random.randint(-sys.maxsize-1, sys.maxsize)
-            unique_id = False
-            while unique_id == False:
-                cursor.execute("Select * FROM home_gds_files WHERE id = %s", [rand_id])
-                info = cursor.fetchall()
-                if info == []:
-                    unique_id = True
-
-            for detection in detections:
-                sample_file = sample_images()
-                sample_file.gds_file_id = id
-                sample_file.file_name = file.name
-                sample_file.width = im.shape[1]
-                sample_file.height = im.shape[0]
-                if "." not in detection.payload:
-                    sample_file.row, sample_file.col = int(re.search( ",(.*):U=UL",detection.payload).group(1)), int(detection.payload.split(",")[0])
-                    sample_file.abs_x = sample_file.row * (qr_size + spacing) + padding
-                    sample_file.abs_y = sample_file.col * (qr_size + spacing) + padding
-
+            print(zip_ref.infolist())
+            for path in zip_ref.infolist():
+                print(path.filename)
+                if Path(path.filename).suffix not in [".png", ".jpeg", ".jpg", ".jp2"]:
+                    os.remove(directory_path + path.filename)
                 else:
-                    sample_file.abs_x, sample_file.abs_y = int(re.search(",(.*):U=")),int(detection.payload.split(",")[0])
-                    sample_file.row = (sample_file.abs_x - padding) / (qr_size + spacing)
-                    sample_file.col = (sample_file.abs_y - padding) / (qr_size + spacing)
-                sample_file.num_codes = len(detections)
-                sample_file.img_id = rand_id # unique id for each image that is the same regardless of which qr code is stored
-                sample_file.save()
+                    print("addding")
+                    add_sample_to_db(path.filename, id, directory_path)
+
+    else:
+        if Path(file.name).suffix in [".png", ".jpeg", ".jpg", ".jp2"]:
+            with open(directory_path + file.name, 'wb+') as f:
+                for chunk in file.chunks():
+                    f.write(chunk)
+        add_sample_to_db(file.name, id, directory_path)
     with connection.cursor() as cursor:
         cursor.execute("UPDATE home_gds_files SET last_updated = %s WHERE id = %s", [datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), int(id)])
 
